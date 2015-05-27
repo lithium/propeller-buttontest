a81948eb7d21d667ce98abb9e381825fde9717cd
lithium <code@hlidskialf.com>
2015-05-26 22:01:35 -0700

    8 button input

--- main.c
+++ main.c
@@ -3,18 +3,22 @@
 #include <propeller.h>
 #include "bithelper.h"
 
+
 int main(void)
 {
 
-  pinOutput(16);
-  pinInput(0);
-  bitSet(OUTA, 16);
+  //bits 0..7 touch button inputs
+  DIRA = 0xffffff00;
+
+  //bits 16..23 lED outputs
+#define led_mask 0x00ff0000
+
 
   while (1) {
-    if (bit(INA, 0)) {
-      bitSet(OUTA, 16);
-    } else {
-      bitClear(OUTA, 16);
-    }
+    // read P0..P7
+    int buttons = INA & 0xFF;
+
+    // write buttons to P16..P23 and leave other bits untouched
+    OUTA = (OUTA & ~led_mask) | buttons<<16;
   }
 }
